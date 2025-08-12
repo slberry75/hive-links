bb29f7647d92660b19d483e5324f100887db5afc
slberry75 <sberry@gmail.com>
2025-08-12 12:49:18 -0400

    Doing some code reorganization in preparation for puzzle-generator

---
+++ src/utils/coordinates.ts
@@ -0,0 +1,49 @@
+const AxialOffsets =  [
+    [1, 0, -1], // Right        
+    [1, -1, 0], // Top Right  
+    [0, -1, 1], // Top Left
+    [-1, 0, 1], // Left   
+    [-1, 1, 0], // Bottom Left
+    [0, 1, -1]  // Bottom Right
+];
+
+export class AxialCoordinates {
+    // properties
+    readonly q: number;
+    readonly r: number;
+    readonly s: number;
+
+    constructor(coords:[number,number,number]);
+    constructor(q:number,r:number,s:number);
+    constructor(qOrCoords:[number,number,number]|number, r?: number, s?: number) {
+        if (Array.isArray(qOrCoords)) 
+        {
+            r = qOrCoords[1];
+            s = qOrCoords[2];
+            qOrCoords = qOrCoords[0];
+        }
+          
+        if (r === undefined || s === undefined)  
+        {
+            throw new Error("Invalid instantiation of Axial Coordinates");
+        }
+        else if (Math.abs(qOrCoords + r + s) > 0.0001)
+        {
+            throw new Error(`Invalid axial coordinates: ${[qOrCoords, r, s]}`);
+        }
+        else
+        {
+            this.q = qOrCoords;
+            this.r = r;
+            this.s = s;   
+        }
+    }
+
+    toArray():[number,number,number] {
+        return [this.q, this.r, this.s]
+    }
+
+    toString():string {
+        return JSON.stringify(this.toArray());
+    }
+}
